f2c1d5dddbe7f41f0e9477e641558f6e6e9c7d17
mancis4 <verdoncrann@hotmail.co.uk>
2022-01-14 18:18:23 +0000

    added split layout on large screens

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:adaptive_layout/adaptive_layout.dart';
 
 import 'package:helixio_app/widgets/menu.dart';
 
@@ -12,6 +13,34 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: MainMenu());
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Helix.io'),
+      ),
+      // Now using an `AdaptiveLayout` as the `body`
+      body: AdaptiveLayout(
+        // Provide `MovieListView` as the `smallLayout`
+        smallLayout: const MainMenu(),
+        // Provide a `Row` as the `largeLayout`
+        largeLayout: Row(
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            const Expanded(flex: 1, child: MainMenu()),
+            const Expanded(
+              flex: 3,
+              child: Center(
+                child: Text(
+                  "Select a menu item on the left to see the page here.",
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
+
+
+
+//return const Scaffold(body: MainMenu());
